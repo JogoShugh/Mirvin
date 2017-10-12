73465ad3f4211e4884df1245a79121f456e87afc
jogoshugh <jsgough@gmail.com>
2017-10-12 16:22:20 -0400

    Add project files.

---
+++ Library/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+	public delegate void MyDel(string arg);
+	public delegate string FuncDel(string arg, int num);
+	public class Test
+    {
+	    public string Name { get; set; }
+	    public Test(string name)
+	    {
+		    Name = name;
+	    }
+
+	    public void CallSomething(MyDel callback)
+	    {
+		    callback(Name);
+	    }
+
+	    public string CallFunc(Func<string, int, string> someFunc) => someFunc(Name, 5);
+		public string CallFuncDel(FuncDel someFunc) => someFunc(Name, 5);
+
+
+	}
+}
---
+++ MarkCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Library;
+using static System.Console;
+
+namespace MarkCSharp
+{
+	class Program
+	{
+		static void CallConsole(string arg) => WriteLine(arg.ToUpper());
+
+		static string JoinArgWithNumber(string arg, int num) => arg + num * num;
+		static void Main(string[] args)
+		{
+			var t = new Test("My Test Instance");
+
+			//MyDel act = CallConsole;
+
+			t.CallSomething(CallConsole);
+
+			var result = t.CallFunc((name, count) =>
+			{
+				var newName = name + count.ToString();
+				return newName;
+			});
+			WriteLine(result);
+
+			Func<string, int, string> myFunc = JoinArgWithNumber;
+			
+			WriteLine(t.CallFunc(JoinArgWithNumber));
+			WriteLine(t.CallFuncDel(JoinArgWithNumber));
+			WriteLine(t.CallFuncDel((name, count) =>
+			{
+				var newName = count + name;
+				return newName;
+			}));
+			WriteLine(t.CallFunc(myFunc));
+
+			Read();
+		}
+	}
+}
